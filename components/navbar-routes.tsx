27ea31fba19dd643fcfbb299e9b1df2f48440015
ss12932 <ss12932@my.bristol.ac.uk>
2023-09-22 11:56:02 +0100

    added dynamic routes for teacher/guest

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { UserButton } from "@clerk/clerk-react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 
 export const NavbarRoutes = () => {
   const pathname = usePathname();
-  const router = useRouter();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
@@ -16,10 +15,12 @@ export const NavbarRoutes = () => {
   return (
     <div className="flex gap-x-2 ml-auto">
       {isTeacherPage || isPlayerPage ? (
-        <Button>
-          <LogOut className="h-4 w-4 mr-2" />
-          Exit
-        </Button>
+        <Link href="/">
+          <Button>
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
       ) : (
         <Link href="/teacher/courses">
           <Button size="sm" variant="ghost">
