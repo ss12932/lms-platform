44ce3e8593e830e5d50443b6c810d3ddd4d43293
ss12932 <ss12932@my.bristol.ac.uk>
2023-09-29 11:47:41 +0100

    api route for individual course + iconbadge + prisma setup

---
+++ lib/db.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NDDE_ENV !== "production") globalThis.prisma = db;
