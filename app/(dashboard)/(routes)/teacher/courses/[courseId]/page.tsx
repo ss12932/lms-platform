9dea0a0e5ca128e9cdc6c24f8756df9178534350
ss12932 <ss12932@my.bristol.ac.uk>
2023-09-29 11:51:47 +0100

    cva setup iconbadge

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,7 @@
+import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
@@ -44,6 +46,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
         <div>
           <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
             <h2 className="text-xl">Customise your course</h2>
           </div>
         </div>
--- components/icon-badge.tsx
+++ components/icon-badge.tsx
@@ -11,10 +11,6 @@ const backgroundVariants = cva(
         default: "bg-sky-100",
         success: "bg-emerald-100",
       },
-      iconVariant: {
-        default: "text-sky-700",
-        success: "text-emerald-700",
-      },
       size: {
         default: "p-2",
         sm: "p-1",
@@ -50,4 +46,10 @@ interface IconBadgeProps extends backgroundVariantProps, IconVariantProps {
   icon: LucideIcon;
 }
 
-export const IconBadge = ({ icon: Icon, variant, size }: IconBadgeProps) => {};
+export const IconBadge = ({ icon: Icon, variant, size }: IconBadgeProps) => {
+  return (
+    <div className={cn(backgroundVariants({ variant, size }))}>
+      <Icon className={cn(iconVariants({ variant, size }))} />
+    </div>
+  );
+};
